442b5378347eca9c1dc6b20f479e58b2ae86c028
superrob3000 <robbracero@gmail.com>
2022-05-03 19:15:12 -0400

    Add project files.

---
+++ OmegaCrypto/OmegaCryptoForm.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace OmegaCrypto
+{
+    partial class OmegaCryptoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.genkeys_button = new System.Windows.Forms.Button();
+            this.Public_textbox = new System.Windows.Forms.TextBox();
+            this.Private_textbox = new System.Windows.Forms.TextBox();
+            this.Public_label = new System.Windows.Forms.Label();
+            this.Private_label = new System.Windows.Forms.Label();
+            this.sign_button = new System.Windows.Forms.Button();
+            this.verify_button = new System.Windows.Forms.Button();
+            this.loadkeys_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // genkeys_button
+            // 
+            this.genkeys_button.Location = new System.Drawing.Point(215, 415);
+            this.genkeys_button.Name = "genkeys_button";
+            this.genkeys_button.Size = new System.Drawing.Size(145, 23);
+            this.genkeys_button.TabIndex = 0;
+            this.genkeys_button.Text = "Generate New Keys";
+            this.genkeys_button.UseVisualStyleBackColor = true;
+            this.genkeys_button.Click += new System.EventHandler(this.genkeys_button_Click);
+            // 
+            // Public_textbox
+            // 
+            this.Public_textbox.Location = new System.Drawing.Point(50, 35);
+            this.Public_textbox.Multiline = true;
+            this.Public_textbox.Name = "Public_textbox";
+            this.Public_textbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Public_textbox.Size = new System.Drawing.Size(672, 157);
+            this.Public_textbox.TabIndex = 1;
+            // 
+            // Private_textbox
+            // 
+            this.Private_textbox.Location = new System.Drawing.Point(50, 243);
+            this.Private_textbox.Multiline = true;
+            this.Private_textbox.Name = "Private_textbox";
+            this.Private_textbox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Private_textbox.Size = new System.Drawing.Size(672, 157);
+            this.Private_textbox.TabIndex = 2;
+            // 
+            // Public_label
+            // 
+            this.Public_label.AutoSize = true;
+            this.Public_label.Location = new System.Drawing.Point(50, 13);
+            this.Public_label.Name = "Public_label";
+            this.Public_label.Size = new System.Drawing.Size(36, 13);
+            this.Public_label.TabIndex = 3;
+            this.Public_label.Text = "Public";
+            // 
+            // Private_label
+            // 
+            this.Private_label.AutoSize = true;
+            this.Private_label.Location = new System.Drawing.Point(50, 218);
+            this.Private_label.Name = "Private_label";
+            this.Private_label.Size = new System.Drawing.Size(40, 13);
+            this.Private_label.TabIndex = 4;
+            this.Private_label.Text = "Private";
+            // 
+            // sign_button
+            // 
+            this.sign_button.Location = new System.Drawing.Point(566, 415);
+            this.sign_button.Name = "sign_button";
+            this.sign_button.Size = new System.Drawing.Size(75, 23);
+            this.sign_button.TabIndex = 5;
+            this.sign_button.Text = "Sign File";
+            this.sign_button.UseVisualStyleBackColor = true;
+            this.sign_button.Click += new System.EventHandler(this.sign_button_Click);
+            // 
+            // verify_button
+            // 
+            this.verify_button.Location = new System.Drawing.Point(647, 415);
+            this.verify_button.Name = "verify_button";
+            this.verify_button.Size = new System.Drawing.Size(75, 23);
+            this.verify_button.TabIndex = 6;
+            this.verify_button.Text = "Verify File";
+            this.verify_button.UseVisualStyleBackColor = true;
+            this.verify_button.Click += new System.EventHandler(this.verify_button_Click);
+            // 
+            // loadkeys_button
+            // 
+            this.loadkeys_button.Location = new System.Drawing.Point(53, 415);
+            this.loadkeys_button.Name = "loadkeys_button";
+            this.loadkeys_button.Size = new System.Drawing.Size(156, 23);
+            this.loadkeys_button.TabIndex = 7;
+            this.loadkeys_button.Text = "Load Keys From File";
+            this.loadkeys_button.UseVisualStyleBackColor = true;
+            this.loadkeys_button.Click += new System.EventHandler(this.loadkeys_button_Click);
+            // 
+            // OmegaCryptoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loadkeys_button);
+            this.Controls.Add(this.verify_button);
+            this.Controls.Add(this.sign_button);
+            this.Controls.Add(this.Private_label);
+            this.Controls.Add(this.Public_label);
+            this.Controls.Add(this.Private_textbox);
+            this.Controls.Add(this.Public_textbox);
+            this.Controls.Add(this.genkeys_button);
+            this.Name = "OmegaCryptoForm";
+            this.Text = "OmegaCrypto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button genkeys_button;
+        private System.Windows.Forms.TextBox Public_textbox;
+        private System.Windows.Forms.TextBox Private_textbox;
+        private System.Windows.Forms.Label Public_label;
+        private System.Windows.Forms.Label Private_label;
+        private System.Windows.Forms.Button sign_button;
+        private System.Windows.Forms.Button verify_button;
+        private System.Windows.Forms.Button loadkeys_button;
+    }
+}
+
---
+++ OmegaCrypto/OmegaCryptoForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OmegaCrypto
+{
+    public partial class OmegaCryptoForm : Form
+    {
+        public OmegaCryptoForm()
+        {
+            InitializeComponent();
+
+            //Load default public/private keys if present
+            String filePath = @"C:\Users\User\Documents\projects\arcade\LaunchBox\OmegaSettingsMenu\rsa\private.txt";
+            if (File.Exists(filePath))
+            {
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.FromXmlString(File.ReadAllText(filePath));
+
+                this.Public_textbox.Text = rsa.ToXmlString(false);
+                this.Private_textbox.Text = rsa.ToXmlString(true);
+            }
+        }
+
+        private void genkeys_button_Click(object sender, EventArgs e)
+        {
+            //Generate a public/private key pair.  
+            RSA rsa = RSA.Create();
+
+            this.Public_textbox.Text = rsa.ToXmlString(false);
+            this.Private_textbox.Text = rsa.ToXmlString(true);
+
+
+        }
+
+        private void sign_button_Click(object sender, EventArgs e)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(Private_textbox.Text);
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                String filePath = openFileDialog.FileName;
+
+                if (!File.Exists(filePath))
+                {
+                    MessageBox.Show("File not found.");
+                    return;
+                }
+
+                //Hash the file
+                byte[] hashValue;
+                byte[] signedHashValue;
+                using (FileStream fs = new FileStream(filePath, FileMode.Open))
+                using (BufferedStream bs = new BufferedStream(fs))
+                {
+                    using (SHA1Managed sha1 = new SHA1Managed())
+                    {
+                        hashValue = sha1.ComputeHash(bs);
+                    }
+                }
+
+                //Sign the hash using the private key:
+
+                //Create an RSAPKCS1SignatureFormatter object and pass it the
+                //RSA instance to transfer the private key.
+                RSAPKCS1SignatureFormatter rsaFormatter = new RSAPKCS1SignatureFormatter(rsa);
+
+                //Set the hash algorithm to SHA1.
+                rsaFormatter.SetHashAlgorithm("SHA1");
+
+                //Create a signature for hashValue and assign it to
+                //signedHashValue.
+                signedHashValue = rsaFormatter.CreateSignature(hashValue);
+
+                //Write the signature to a file
+                if (File.Exists(filePath + ".signature"))
+                    File.Delete(filePath + ".signature");
+                File.WriteAllBytes(filePath + ".signature", signedHashValue);
+
+                MessageBox.Show("Signature written to " + filePath + ".signature");
+            }
+        }
+
+        private void verify_button_Click(object sender, EventArgs e)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(Public_textbox.Text);
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                String filePath = openFileDialog.FileName;
+
+                if (!File.Exists(filePath))
+                {
+                    MessageBox.Show("File not found.");
+                    return;
+                }
+
+                if (!File.Exists(filePath + ".signature"))
+                {
+                    MessageBox.Show("Signature not found.");
+                    return;
+                }
+
+
+                //Hash the file
+                byte[] hashValue;
+                byte[] signedHashValue = File.ReadAllBytes(filePath + ".signature");
+                using (FileStream fs = new FileStream(filePath, FileMode.Open))
+                using (BufferedStream bs = new BufferedStream(fs))
+                {
+                    using (SHA1Managed sha1 = new SHA1Managed())
+                    {
+                        hashValue = sha1.ComputeHash(bs);
+                    }
+                }
+
+                //Verify the hash using the public key:
+
+                //Create an RSAPKCS1SignatureDeformatter object and pass it the
+                //RSA instance to transfer the public key.
+                RSAPKCS1SignatureDeformatter rsaDeformatter = new RSAPKCS1SignatureDeformatter(rsa);
+
+                //Set the hash algorithm to SHA1.
+                rsaDeformatter.SetHashAlgorithm("SHA1");
+
+                if (rsaDeformatter.VerifySignature(hashValue, signedHashValue))
+                {
+                    MessageBox.Show("The signature is valid.");
+                }
+                else
+                {
+                    MessageBox.Show("The signature is not valid!!!");
+                }
+            }
+        }
+
+        private void loadkeys_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                String filePath = openFileDialog.FileName;
+
+                if (!File.Exists(filePath))
+                {
+                    MessageBox.Show("File not found.");
+                    return;
+                }
+
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.FromXmlString(File.ReadAllText(filePath));
+
+                this.Public_textbox.Text = rsa.ToXmlString(false);
+                this.Private_textbox.Text = rsa.ToXmlString(true);
+
+            }
+        }
+    }
+}
